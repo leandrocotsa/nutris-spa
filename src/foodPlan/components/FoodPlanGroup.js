5d54e631fbad25f44f1e30eb4ecf44e2ce75aa79
leandrocotsa <leandrocotsa@hotmail.com>
2022-02-27 12:12:42 +0000

    food plan half done

--- src/foodPlan/components/FoodPlanGroup.js
+++ src/foodPlan/components/FoodPlanGroup.js
@@ -12,53 +12,52 @@ import { useLocation } from 'react-router-dom';
 
 const FoodPlanGroup = props => {
 
-    console.log(props.foodPLan);
-
     //state com array do food plan em que cada vez que muda é recalculada stuff
     //o componente com as barras usa esse state como props
     //sempre que acrescentar ou retirar um alimento o activeMacroNutrients muda de valores
     //eu so consigo editar o ativo portanto há match entre o aberto e o que tem as barras
 
-    const [foodPlan, setFoodPlan] = useState(props.foodPlan);
+    const [foodDiaries, setFoodDiaries] = useState([]);
 
-    const [planMacroNutrients, setPlanMacroNutrients] = useState(props.foodPlan);
+    const [selectedFoodDiary, setSelectedFoodDiary] = useState([]);
+
+
 
 
     const location = useLocation();
     const { patient } = location.state;
 
-    useEffect(() => {
-
-        //recalculate
-
-        //setPlanMacroNutrients
-
-    }, []); //plan aliments
-
-
-    const patientProtein = patient.macroNutrients.proteins;
-    const patientFat = patient.macroNutrients.fat;
-    const patientHydrates = patient.macroNutrients.hydrates;
 
 
     //edit food plan logic
     useEffect(() => {
-        setFoodPlan(props.foodPlan);
-    }, [props.foodPlan])
+
+        console.log(foodDiaries)
+        //recalculate
+
+        //setPlanMacroNutrients
+    }, [foodDiaries])
 
 
-    const addDayHandler = () => {
+    const addFoodDiaryHandler = () => {
 
-        setFoodPlan((prevFoodPlan) => {
-            return prevFoodPlan.concat({ "id": 3 });
-        })
+        setFoodDiaries([...foodDiaries,
+        {
+            id: Math.random(),
+            totalFat: 0,
+            totalHydrates: 0,
+            totalKcal: 0,
+            totalProtein: Math.floor(Math.random() * 31)
+        }])
 
     }
 
+    const removeFoodDiaryHandler = () => {
 
-
-
-    if (foodPlan.length === 0) {
+        setFoodDiaries((prevFoodDiaries, index) => {
+            prevFoodDiaries.splice(1, index);
+            return prevFoodDiaries;
+        })
 
     }
 
@@ -75,7 +74,7 @@ const FoodPlanGroup = props => {
                 <div className="food-plan-group__left-column">
                     <Card className='food-plan-card'>
                         <div className='food-plan-card-header'>
-                            <Button color='teal' variant="light" radius="md" compact onClick={addDayHandler}>
+                            <Button color='teal' variant="light" radius="md" compact onClick={addFoodDiaryHandler}>
                                 + New day plan
                             </Button>
                         </div>
@@ -84,10 +83,17 @@ const FoodPlanGroup = props => {
 
 
 
-                            {foodPlan.map(day => (
-                                <Accordion.Item label={<h3>Day plan {day.id}</h3>} onClick={console.log("clicked")}>
-                                    <h1>Lol</h1>
-                                    <h1>Lol</h1><h1>Lol</h1><h1>Lol</h1><h1>Lol</h1><h1>Lol</h1><h1>Lol</h1><h1>Lol</h1><h1>Lol</h1><h1>Lol</h1><h1>Lol</h1><h1>Lol</h1><h1>Lol</h1><h1>Lol</h1><h1>Lol</h1><h1>Lol</h1><h1>Lol</h1><h1>Lol</h1><h1>Lol</h1><h1>Lol</h1>
+                            {foodDiaries.map((foodDiary, index) => (
+                                <Accordion.Item label={<h3>Day plan {index} </h3>} onClick={() => setSelectedFoodDiary(foodDiary)}>
+                                    <Button onClick={() => {
+                                        setFoodDiaries((prevFoodDiaries) => {
+                                            prevFoodDiaries.splice(index, 1);
+                                            const newArray = [...prevFoodDiaries]
+                                            return newArray;
+                                        })
+                                    }}>Delete</Button>
+                                    <h1>{foodDiary.id}</h1>
+                                    <h1>Lol</h1><h1>Lol</h1><h1>Lol</h1>
                                 </Accordion.Item>
                             ))}
 
@@ -105,6 +111,9 @@ const FoodPlanGroup = props => {
                         <div className='ref-values-card-header'>
                             <h2>Reference macro-nutrients values</h2>
                         </div>
+
+
+
                         <div className='ref-values-card__container'>
                             <div className="ref-values-item">
                                 <h4><span className='ref-value'>Daily protein amount: </span>{patient.macroNutrients.proteins.toFixed(2)} g</h4>
@@ -125,6 +134,13 @@ const FoodPlanGroup = props => {
                         </div>
 
 
+
+
+
+
+
+
+
                     </Card>
 
                     <Card className='progress-card'>
@@ -132,17 +148,36 @@ const FoodPlanGroup = props => {
                             <h2>Macro-nutrients state</h2>
                         </div>
 
-                        <div>
-                            <h3>Day Plan 1</h3>
-                            <h4>Protein</h4>
-                            <Progress value={75} label="75g" size="xl" radius="xl" color="green" />
-                            <h4>Fat</h4>
-                            <Progress value={32} label="32g" size="xl" radius="xl" color="lime" />
-                            <h4>Carbohydrates</h4>
-                            <Progress value={49} label="48g" size="xl" radius="xl" color="cyan" />
-                            <h4>Calories</h4>
-                            <Progress value={92} label="1563 kcal" size="xl" radius="xl" color="teal" />
-                        </div>
+
+                        {selectedFoodDiary.id ?
+                            <div>
+                                <h3>Day Plan 1</h3>
+                                <p>lol{selectedFoodDiary.id}</p>
+                                <h4>Protein</h4>
+                                {selectedFoodDiary.totalProtein < patient.macroNutrients.proteins
+                                    ? <Progress value={parseInt((selectedFoodDiary.totalProtein / patient.macroNutrients.proteins) * 100)} label={selectedFoodDiary.totalProtein + "g"} size="xl" radius="xl" color="green" />
+                                    :
+                                    <React.Fragment>
+                                        <Progress value={100} label={selectedFoodDiary.totalProtein + "g"} size="xl" radius="xl" color="red" />
+                                        <p>Exceeded</p>
+                                    </React.Fragment>
+                                }
+
+                                <h4>Fat</h4>
+                                <Progress value={32} label="32g" size="xl" radius="xl" color="lime" />
+                                <h4>Carbohydrates</h4>
+                                <Progress value={49} label="48g" size="xl" radius="xl" color="cyan" />
+                                <h4>Calories</h4>
+                                <Progress value={92} label="1563 kcal" size="xl" radius="xl" color="teal" />
+                            </div>
+
+                            : <p>No selected Food Diary</p>
+
+
+
+                        }
+
+
                     </Card>
                 </div>
 
